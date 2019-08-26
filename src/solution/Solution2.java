c93ff23723a8c796bdec195c8cdce6d11a639473
Wang_Ziren <245107137@qq.com>
2019-08-26 10:23:53 +0800

    [add]添加题2的解法思路

--- src/solution/Solution2.java
+++ src/solution/Solution2.java
@@ -1,6 +1,12 @@
 package solution;
 
 public class Solution2 {
+    /*
+    本题的关键在于每步对链表的操作都是重复的
+    因此用while循环来进行，另一个问题是开始状态和结束条件的判断问题
+    使用虚拟头结点作为开始的状态，结束条件需要注意进位为1的情况，
+    另一个需要注意的问题是原来两个链表的指针需要移动到next
+    */
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int carry = 0;
         ListNode node1 = new ListNode(0);
